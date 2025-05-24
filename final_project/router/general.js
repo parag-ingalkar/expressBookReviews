21317afadbc88eab857ddd9c0c463eabb58693e1
parag-ingalkar <paragingalkar@gmail.com>
2025-05-24 09:44:34 -0400

    Implemented routes for general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,25 +19,59 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const keys = Object.keys(books);
+  let matchingBooks = [];
+
+  keys.forEach((key) => {
+    if (books[key].author === author) {
+        matchingBooks.push(books[key]);
+    } 
+  });
+
+  if (matchingBooks.length > 0) {
+    res.send(matchingBooks);
+  } else {
+res.status(404).json({message:"No books found by given author"});
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const keys = Object.keys(books);
+  let matchingBooks = [];
+
+  keys.forEach((key) => {
+    if (books[key].title === title) {
+        matchingBooks.push(books[key]);
+    } 
+  });
+
+  if (matchingBooks.length > 0) {
+    res.send(matchingBooks);
+  } else {
+res.status(404).json({message:"No books found by given title"});
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if (books[isbn]){
+      res.send(books[isbn].reviews);
+  } else {
+    res.status(404).json({message : "No book available with given ISBN"})
+  }
 });
 
 module.exports.general = public_users;
